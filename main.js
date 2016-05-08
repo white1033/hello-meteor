e36fb4b2978a31d9d7fec357d67a6c8ed55d601d
white1033 <k75715@gmail.com>
2016-05-08 16:46:46 +0800

    add users and use Meteor call

--- main.js
+++ main.js
@@ -9,20 +9,24 @@ if (Meteor.isClient) {
 
     Template.body.events({
         "submit form": function(e, t) {
+            e.preventDefault();
             let text = $(e.target).find("#inputMessage").val();
-            let user = $(e.target).find("#inputUser").val();
-            $("form > input").val("");
-            if (user === "") {
-                user = "aya";
-            }
-            let message = {
-                text,
-                user,
-                from: "db",
-                createdAt: new Date(),
-            };
-            Messages.insert(message);
+            $("input").val("");
+            let message = {text};
+            Meteor.call("createdMessage", message);
         }
     })
 }
 
+if (Meteor.isServer) {
+    Meteor.methods({
+        createdMessage(message) {
+            let user = Meteor.userId();
+            if (user) {
+                message.user = Meteor.user().emails[0].address;
+                message.createdAt = new Date();
+            }
+            Messages.insert(message);
+        }
+    })
+}
