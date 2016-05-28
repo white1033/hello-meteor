62fbe739012b1225bde970f38f0bfa87bdb69acb
white1033 <k75715@gmail.com>
2016-05-28 09:29:45 +0800

    add iron:router

--- main.js
+++ main.js
@@ -1,40 +1,46 @@
-Messages = new Mongo.Collection("messages");
+Messages = new Mongo.Collection("messages")
+
+// Router.configure({
+//     layoutTemplate: 'layout'
+// })
+Router.route('/', function() {
+    this.render('guestBook')
+})
 
 if (Meteor.isClient) {
     Meteor.startup(function() {
         Meteor.subscribe('lastTenMessages', 20)
     })
 
-    Template.body.helpers({
+    Template.guestBook.helpers({
         messages() {
-            return Messages.find({}, {sort: {createdAt: -1}});
+            return Messages.find({}, {sort: {createdAt: -1}})
         }
     })
 
-    Template.body.events({
+    Template.guestBook.events({
         "submit form": function(e, t) {
-            e.preventDefault();
-            let text = $(e.target).find("#inputMessage").val();
-            $("input").val("");
-            let message = {text};
-            Meteor.call("createdMessage", message);
+            e.preventDefault()
+            let text = $(e.target).find("#inputMessage").val()
+            $("input").val("")
+            let message = {text}
+            Meteor.call("createdMessage", message)
         }
     })
 }
 
 if (Meteor.isServer) {
     Meteor.publish('lastTenMessages', function(limit) {
-        // console.log(this.userId)
         return Messages.find({}, {sort: {createdAt: -1}, limit})
     })
 
     Meteor.methods({
         createdMessage(message) {
-            let user = Meteor.userId();
+            let user = Meteor.userId()
             if (user) {
-                message.user = Meteor.user().emails[0].address;
-                message.createdAt = new Date();
-                Messages.insert(message);
+                message.user = Meteor.user().emails[0].address
+                message.createdAt = new Date()
+                Messages.insert(message)
             }
         }
     })
